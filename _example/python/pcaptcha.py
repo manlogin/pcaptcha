c896f3a0933be9b2cbd3fee50d60450676371f3b
javadnasrollahi <javadnasrollahi68@gmail.com>
2020-02-19 10:40:18 +0330

    convert http to https

--- _example/python/pcaptcha.py
+++ _example/python/pcaptcha.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     pcaptcha 	= "ee507ff105c95a41b21716a092edc5f6e497d83189b65d5ca1789e34da804206cf0c8dbff4fbac72acb7870e79c1cfc07cc39cca0cc03b5b07b70a0c906bac1b" # مقداری است که از طریق فرم ارسال شده است
     uid 		= "0x4e2e" # کد یکتای کپچای شما در سایت من لاکین
     secretKey 	= "e8414ae66a37806bae1166a6297ce302a393e70b8e444b37abe20fe4e277779c" # کلید خصوصی
-    url         = "http://manlogin.com/captcha/cheack/v1/"+uid+"/"+secretKey+"/"+str(pcaptcha)
+    url         = "https://manlogin.com/captcha/cheack/v1/"+uid+"/"+secretKey+"/"+str(pcaptcha)
     r = s.get(url)
     if r.status_code == 200:
         json = json.loads(r.text)
